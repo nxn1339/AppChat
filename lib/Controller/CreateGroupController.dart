ce9829a7414b1f54eddf9928d598cdda69cdb6c8
ngatnx <nxn1339@gmail.com>
2024-04-22 23:27:16 +0700

    create group p2

--- lib/Controller/CreateGroupController.dart
+++ lib/Controller/CreateGroupController.dart
@@ -1,15 +1,27 @@
 import 'package:chat_app/Model/MDUser.dart';
 import 'package:chat_app/Service/APICaller.dart';
+import 'package:chat_app/Utils/Utils.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CreateGroupController extends GetxController {
   RxList<MDUser> listUser = RxList<MDUser>();
   RxList<String> listIDUser = RxList<String>();
-
+  List<String> listIDUserInGroup = [];
+  TextEditingController textEditNameGroup = TextEditingController();
+  String uuid = '';
   @override
   void onInit() {
     super.onInit();
     fecthGroup();
+    loadSavedText();
+  }
+
+  loadSavedText() async {
+    if (await Utils.getStringValueWithKey('id') != '' ||
+        await Utils.getStringValueWithKey('id') != null) {
+      uuid = await Utils.getStringValueWithKey('id');
+    }
   }
 
   void addMember(String id, int index) {
@@ -19,6 +31,7 @@ class CreateGroupController extends GetxController {
       listIDUser[index] = id;
     }
     listIDUser.refresh();
+    listIDUserInGroup = listIDUser;
   }
 
   void fecthGroup() async {
@@ -30,7 +43,49 @@ class CreateGroupController extends GetxController {
             list.map((dynamic json) => MDUser.fromJson(json)).toList();
         listUser.addAll(listItem);
         listUser.refresh();
+        listUser.removeWhere((element) => element.id == uuid);
         listIDUser = RxList.generate(listUser.length, (index) => '');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> addMemberInGroup(String idGroup) async {
+    await Future.forEach(listIDUserInGroup, (element) async {
+      if (element.isEmpty) {
+        listIDUserInGroup.remove(element);
+      }
+    });
+    try {
+      for (var i = 0; i < listIDUserInGroup.length; i++) {
+        var body = {
+          "id_user": listIDUserInGroup[i],
+          "id_group": idGroup,
+        };
+        var response = await APICaller.getInstance().post('group/member', body);
+        if (response != null) {
+          print('done');
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void createGroup() async {
+    var body = {
+      "name": textEditNameGroup.text,
+      "image": "",
+      "id_user": uuid,
+    };
+    try {
+      var response = await APICaller.getInstance().post('group', body);
+      if (response != null) {
+        await addMemberInGroup(response['data']['id']);
+        Get.back();
+        Utils.showSnackBar(
+            title: "Thông báo", message: 'Thêm thành công Group');
       }
     } catch (e) {
       print(e);
--- lib/View/Group/CreateGroup.dart
+++ lib/View/Group/CreateGroup.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/Controller/CreateGroupController.dart';
 import 'package:chat_app/Utils/UtilColor.dart';
+import 'package:chat_app/Utils/UtilLink.dart';
 import 'package:chat_app/Utils/Utils.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -18,99 +19,112 @@ class CreateGroup extends StatelessWidget {
         elevation: 0.5,
         title: const Text('Tạo nhóm mới'),
         actions: [
-          Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              child: Center(
-                  child: Text(
-                'Tạo',
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                    color: UtilColor.textBase),
-              )))
-        ],
-      ),
-      body: Column(
-        children: [
-          Utils.textField(
-              icon: const Icon(Icons.group),
-              hintText: 'Tên nhóm....',
-              maxLines: 1),
-          const SizedBox(
-            height: 10,
-          ),
-          Utils.textFieldCustom(
-              maxLines: 1,
-              icon: const Icon(Icons.search),
-              hintText: 'Tìm kiếm'),
-          const SizedBox(
-            height: 10,
-          ),
-          Obx(
-            () => controller.listUser.isEmpty
-                ? Utils.noData()
-                : Expanded(
-                    child: ListView.builder(
-                      itemCount: controller.listUser.length,
-                      itemBuilder: (context, index) {
-                        return GestureDetector(
-                          onTap: () {
-                            controller.addMember(
-                                controller.listUser[index].id ?? '', index);
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 5),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    ClipOval(
-                                      child: Image.network(
-                                        '${controller.listUser[index].avatar}',
-                                        height: 40,
-                                        width: 40,
-                                        fit: BoxFit.cover,
-                                        errorBuilder:
-                                            (context, error, stackTrace) {
-                                          return Container(
-                                            height: 40,
-                                            width: 40,
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 5, horizontal: 6),
-                                            color: UtilColor.buttonBlue,
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 10,
-                                    ),
-                                    Text(
-                                      controller.listUser[index].name ?? '',
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w500,
-                                          color: UtilColor.textBase),
-                                    )
-                                  ],
-                                ),
-                                Obx(
-                                  () => controller.listIDUser[index] ==
-                                          controller.listUser[index].id
-                                      ? const Icon(Icons.radio_button_checked)
-                                      : const Icon(Icons.radio_button_off),
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+          GestureDetector(
+            onTap: () {
+              controller.createGroup();
+            },
+            child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Center(
+                    child: Text(
+                  'Tạo',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: UtilColor.textBase),
+                ))),
           )
         ],
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Utils.textField(
+                icon: const Icon(Icons.group),
+                hintText: 'Tên nhóm....',
+                maxLines: 1,
+                controller: controller.textEditNameGroup),
+            const SizedBox(
+              height: 10,
+            ),
+            Utils.textFieldCustom(
+                maxLines: 1,
+                icon: const Icon(Icons.search),
+                hintText: 'Tìm kiếm'),
+            const SizedBox(
+              height: 10,
+            ),
+            Obx(
+              () => controller.listUser.isEmpty
+                  ? Utils.noData()
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: controller.listUser.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            onTap: () {
+                              controller.addMember(
+                                  controller.listUser[index].id ?? '', index);
+                            },
+                            child: Container(
+                              color: Colors.white,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      ClipOval(
+                                        child: Image.network(
+                                          '${UtilLink.BASE_URL}${controller.listUser[index].avatar}',
+                                          height: 40,
+                                          width: 40,
+                                          fit: BoxFit.cover,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return Container(
+                                              height: 40,
+                                              width: 40,
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5,
+                                                      horizontal: 6),
+                                              color: UtilColor.buttonBlue,
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        controller.listUser[index].name ?? '',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500,
+                                            color: UtilColor.textBase),
+                                      )
+                                    ],
+                                  ),
+                                  Obx(
+                                    () => controller.listIDUser[index] ==
+                                            controller.listUser[index].id
+                                        ? const Icon(Icons.radio_button_checked)
+                                        : const Icon(Icons.radio_button_off),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+            )
+          ],
+        ),
       ),
     );
   }
